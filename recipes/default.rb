fe9a80543c5af86433f2f4b3d440a9d0ce166ce3
swoerkom <swoerkom@spartaglobal.com>
2019-06-04 09:14:17 +0100

    Adding template creation for requirements file

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,3 +15,11 @@ end
 package("python-minimal") do
   action :install
 end
+
+template '/etc/python/requirements.txt' do
+  source 'app/requirements.txt'
+end
+
+execute 'install requirements' do
+  command 'pip install -r /etc/python/requirements.txt'
+end
